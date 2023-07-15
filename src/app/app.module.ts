5ee237da1f3b1106306ec2f609d7cc616607a9ef
Bharathk <kumar.bharath289@gmail.com>
2023-07-14 21:10:49 -0400

    adding components through ng g c services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule} from '@angular/forms'
+import { FormsModule} from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { SeverComponent } from './server/sever.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SeverComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,FormsModule
